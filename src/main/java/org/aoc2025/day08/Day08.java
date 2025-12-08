66adae0b591b9ca6777ee1f1d3d748d7b54bb5c5
iain.maclean <iain.p.m.maclean@gmail.com>
2025-12-08 19:14:41 +0000

    Some Day08 refactoring and improvement

--- src/main/java/org/aoc2025/day08/Day08.java
+++ src/main/java/org/aoc2025/day08/Day08.java
@@ -41,19 +41,12 @@ public class Day08 {
     private static void solvePartOne() throws IOException {
         final int junctionBoxesToConnect = 1000;
         List<PointLong3D> input = getInput();
-        Map<Point, Long> coordsIndicesToDistanceSquaredMap = new HashMap<>();
-        for (int i = 0; i < input.size() - 1; i++) {
-            for (int j = i + 1; j < input.size(); j++) {
-                long distanceSquared = input.get(i).getSquareOfEuclideanDistance(input.get(j));
-                coordsIndicesToDistanceSquaredMap.put(new Point(i, j), distanceSquared);
-            }
-        }
-        List<Point> distanceSquareOrderedByValues = returnListOfEntriesSortedByValue(coordsIndicesToDistanceSquaredMap);
+        List<Point> coordsIndicesOrderedByDistance = getCoordsIndicesOrderedByDistance(input);
         HashMap<PointLong3D, Integer> junctionBoxToNetworkMap = new HashMap<>();
         for (int i = 0; i < junctionBoxesToConnect; i++) {
-            PointLong3D junctionBox1 = input.get(distanceSquareOrderedByValues.get(i).x);
-            PointLong3D junctionBox2 = input.get(distanceSquareOrderedByValues.get(i).y);
-            connectJunctionBoxes(junctionBox1, junctionBox2, junctionBoxToNetworkMap);
+            PointLong3D junctionBox1 = input.get(coordsIndicesOrderedByDistance.get(i).x);
+            PointLong3D junctionBox2 = input.get(coordsIndicesOrderedByDistance.get(i).y);
+            connectJunctionBoxes(junctionBox1, junctionBox2, junctionBoxToNetworkMap, 0);
         }
         int[] groupedByCircuit = junctionBoxToNetworkMap
                 .entrySet().stream().collect(Collectors.groupingBy(Map.Entry::getValue))
@@ -64,24 +57,15 @@ public class Day08 {
 
     private static void solvePartTwo() throws IOException {
         List<PointLong3D> input = getInput();
-        Map<Point, Long> coordsIndicesToDistanceSquaredMap = new HashMap<>();
-        for (int i = 0; i < input.size() - 1; i++) {
-            for (int j = i + 1; j < input.size(); j++) {
-                long distanceSquared = input.get(i).getSquareOfEuclideanDistance(input.get(j));
-                coordsIndicesToDistanceSquaredMap.put(new Point(i, j), distanceSquared);
-            }
-        }
-        List<Point> distanceSquareOrderedByValues = returnListOfEntriesSortedByValue(coordsIndicesToDistanceSquaredMap);
+        List<Point> coordsIndicesOrderedByDistance = getCoordsIndicesOrderedByDistance(input);
         HashMap<PointLong3D, Integer> junctionBoxToNetworkMap = new HashMap<>();
-        int circuitCount;
+        int circuitCount = 0;
         int counter = 0;
         long solution = -1;
         do {
-            PointLong3D junctionBox1 = input.get(distanceSquareOrderedByValues.get(counter).x);
-            PointLong3D junctionBox2 = input.get(distanceSquareOrderedByValues.get(counter).y);
-            connectJunctionBoxes(junctionBox1, junctionBox2, junctionBoxToNetworkMap);
-            circuitCount = junctionBoxToNetworkMap
-                    .entrySet().stream().collect(Collectors.groupingBy(Map.Entry::getValue)).size();
+            PointLong3D junctionBox1 = input.get(coordsIndicesOrderedByDistance.get(counter).x);
+            PointLong3D junctionBox2 = input.get(coordsIndicesOrderedByDistance.get(counter).y);
+            circuitCount = connectJunctionBoxes(junctionBox1, junctionBox2, junctionBoxToNetworkMap, circuitCount);
             counter++;
             if (circuitCount == 1 && junctionBoxToNetworkMap.size() == input.size()) {
                 solution = junctionBox1.x() * junctionBox2.x();
@@ -91,22 +75,35 @@ public class Day08 {
         System.out.printf("The solution to part two is %s.%n", solution);
     }
 
-    private static void connectJunctionBoxes(PointLong3D junctionBox1, PointLong3D junctionBox2, HashMap<PointLong3D, Integer> junctionBoxToNetworkMap) {
+    private static List<Point> getCoordsIndicesOrderedByDistance(List<PointLong3D> input) {
+        Map<Point, Long> coordsIndicesToDistanceSquaredMap = new HashMap<>();
+        for (int i = 0; i < input.size() - 1; i++) {
+            for (int j = i + 1; j < input.size(); j++) {
+                long distanceSquared = input.get(i).getSquareOfEuclideanDistance(input.get(j));
+                coordsIndicesToDistanceSquaredMap.put(new Point(i, j), distanceSquared);
+            }
+        }
+        return returnListOfEntriesSortedByValue(coordsIndicesToDistanceSquaredMap);
+    }
+
+    private static int connectJunctionBoxes(PointLong3D junctionBox1, PointLong3D junctionBox2, HashMap<PointLong3D, Integer> junctionBoxToNetworkMap, int circuitCount) {
         Optional<Integer> maxCircuitNumber = junctionBoxToNetworkMap.values().stream().max(Integer::compare);
         if (!junctionBoxToNetworkMap.containsKey(junctionBox1) && !junctionBoxToNetworkMap.containsKey(junctionBox2)) {
             int nextCircuitNumber = maxCircuitNumber.map(integer -> integer + 1).orElse(1);
             junctionBoxToNetworkMap.put(junctionBox1, nextCircuitNumber);
             junctionBoxToNetworkMap.put(junctionBox2, nextCircuitNumber);
+            return circuitCount + 1;
         } else if ((junctionBoxToNetworkMap.containsKey(junctionBox1) && !junctionBoxToNetworkMap.containsKey(junctionBox2)) ||
                 (!junctionBoxToNetworkMap.containsKey(junctionBox1) && junctionBoxToNetworkMap.containsKey(junctionBox2))) {
             int circuitNumber = junctionBoxToNetworkMap.getOrDefault(junctionBox1, junctionBoxToNetworkMap.get(junctionBox2));
             junctionBoxToNetworkMap.put(junctionBox1, circuitNumber);
             junctionBoxToNetworkMap.put(junctionBox2, circuitNumber);
+            return circuitCount;
         } else if (junctionBoxToNetworkMap.containsKey(junctionBox1) && junctionBoxToNetworkMap.containsKey(junctionBox2)) {
             int circuitNumber1 = junctionBoxToNetworkMap.get(junctionBox1);
             int circuitNumber2 = junctionBoxToNetworkMap.get(junctionBox2);
             if (circuitNumber1 == circuitNumber2) {
-                return;
+                return circuitCount;
             }
             List<PointLong3D> junctionBoxesInEitherNetwork = junctionBoxToNetworkMap.entrySet().stream()
                     .filter(x -> x.getValue() == circuitNumber1 || x.getValue() == circuitNumber2)
@@ -115,6 +112,7 @@ public class Day08 {
             for (PointLong3D junctionBox : junctionBoxesInEitherNetwork) {
                 junctionBoxToNetworkMap.put(junctionBox, circuitNumber1);
             }
+            return circuitCount - 1;
         } else {
             throw new IllegalStateException("State that I haven't thought of!");
         }
