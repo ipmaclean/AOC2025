9c8845c43548a3bc173e16997d71cd3fe76b6f19
iain.maclean <iain.p.m.maclean@gmail.com>
2025-12-12 11:05:18 +0000

    Solved Day11 part 2

--- src/main/java/org/aoc2025/day11/Day11.java
+++ src/main/java/org/aoc2025/day11/Day11.java
@@ -1,5 +1,7 @@
 package org.aoc2025.day11;
 
+import org.aoc2025.utils.tuple.Tuple4;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -39,8 +41,7 @@ public class Day11 {
                     String match = matcher.group();
                     if (counter++ == 0) {
                         firstMatch = match;
-                    }
-                    else {
+                    } else {
                         otherMatches.add(match);
                     }
                 }
@@ -52,22 +53,54 @@ public class Day11 {
 
     private static void solvePartOne() throws IOException {
         HashMap<String, List<String>> input = getInput();
-        System.out.printf("The solution to part one is %s.%n", countPathsToOut("you", input));
-    }
-
-    private static long countPathsToOut(String currentServer, HashMap<String, List<String>> input) {
-        if (currentServer.equals("out")) {
-            return 1;
-        }
-        long currentCount = 0;
-        for (String server: input.get(currentServer)) {
-            currentCount += countPathsToOut(server, input);
-        }
-        return currentCount;
+        System.out.printf("The solution to part one is %s.%n", countPathsToOut("you", input, new HashMap<>()).x0());
     }
 
     private static void solvePartTwo() throws IOException {
-        long solution = 0;
-        System.out.printf("The solution to part two is %s.%n", solution);
+        HashMap<String, List<String>> input = getInput();
+        System.out.printf("The solution to part two is %s.%n", countPathsToOut("svr", input, new HashMap<>()).x3());
+    }
+
+    // Tuple4<Long total paths, Long pathsHittingDac, Long pathsHittingFFt, Long pathsHittingBoth>
+    private static Tuple4<Long, Long, Long, Long> countPathsToOut(
+            String currentServer,
+            HashMap<String, List<String>> input,
+            HashMap<String, Tuple4<Long, Long, Long, Long>> pathsToOutCache) {
+        if (currentServer.equals("out")) {
+            return new Tuple4<>(1L, 0L, 0L, 0L);
+        }
+        long currentTotal = 0;
+        long currentPathsHittingDac = 0;
+        long currentPathsHittingFFt = 0;
+        long currentPathsHittingBoth = 0;
+        for (String server : input.get(currentServer)) {
+            if (pathsToOutCache.containsKey(server)) {
+                Tuple4<Long, Long, Long, Long> cacheValue = pathsToOutCache.get(server);
+                currentTotal += cacheValue.x0();
+                currentPathsHittingDac += cacheValue.x1();
+                currentPathsHittingFFt += cacheValue.x2();
+                currentPathsHittingBoth += cacheValue.x3();
+            } else {
+                Tuple4<Long, Long, Long, Long> pathsToOut = countPathsToOut(server, input, pathsToOutCache);
+
+                long pathsToOutHittingDac = server.equals("dac") ? pathsToOut.x0() : pathsToOut.x1();
+                long pathsToOutHittingFft = server.equals("fft") ? pathsToOut.x0() : pathsToOut.x2();
+                long pathsToOutHittingBoth;
+                if (server.equals("dac")) {
+                    pathsToOutHittingBoth = pathsToOut.x2();
+                } else if (server.equals("fft")) {
+                    pathsToOutHittingBoth = pathsToOut.x1();
+                } else {
+                    pathsToOutHittingBoth = pathsToOut.x3();
+                }
+
+                currentTotal += pathsToOut.x0();
+                currentPathsHittingDac += pathsToOutHittingDac;
+                currentPathsHittingFFt += pathsToOutHittingFft;
+                currentPathsHittingBoth += pathsToOutHittingBoth;
+                pathsToOutCache.put(server, new Tuple4<>(pathsToOut.x0(), pathsToOutHittingDac, pathsToOutHittingFft, pathsToOutHittingBoth));
+            }
+        }
+        return new Tuple4<>(currentTotal, currentPathsHittingDac, currentPathsHittingFFt, currentPathsHittingBoth);
     }
 }
---
+++ src/main/java/org/aoc2025/utils/tuple/Tuple4.java
@@ -0,0 +1,4 @@
+package org.aoc2025.utils.tuple;
+
+public record Tuple4<X0, X1, X2, X3>(X0 x0, X1 x1, X2 x2, X3 x3) {
+}
