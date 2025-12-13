de4aeecdf40d68946c2420c51ba7f23c0280c8e3
iain.maclean <iain.p.m.maclean@gmail.com>
2025-12-13 22:24:51 +0000

    Solved Day 12 part 1. In a manner of speaking.. works for the input!

--- src/main/java/org/aoc2025/day12/Day12.java
+++ src/main/java/org/aoc2025/day12/Day12.java
@@ -5,11 +5,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day12 {
 
-    private static final String INPUT_FILE_NAME = "day12/example.txt";
+    private static final String INPUT_FILE_NAME = "day12/input.txt";
 
     private Day12() {
         throw new IllegalStateException("Utility class");
@@ -17,29 +20,36 @@ public class Day12 {
 
     public static void solve() throws IOException {
         solvePartOne();
-        solvePartTwo();
     }
 
-    private static List<String> getInput() throws IOException {
+    private static List<PresentInstructions> getInput() throws IOException {
         InputStream inputStream = Day12.class.getClassLoader().getResourceAsStream(INPUT_FILE_NAME);
-        List<String> instructions = new ArrayList<>();
+        List<PresentInstructions> presentInstructions = new ArrayList<>();
+        Pattern pattern = Pattern.compile("^\\d+x\\d");
 
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                instructions.add(line);
+                Matcher matcher = pattern.matcher(line);
+                if (!matcher.find()) {
+                    continue;
+                }
+                presentInstructions.add(new PresentInstructions(line));
             }
         }
-        return instructions;
+        return presentInstructions;
     }
 
     private static void solvePartOne() throws IOException {
         long solution = 0;
+        List<PresentInstructions> presentInstructions = getInput();
+        for (PresentInstructions presentInstruction : presentInstructions) {
+            long gridArea = presentInstruction.getRegionDimensions().x() * presentInstruction.getRegionDimensions().y();
+            long shapeCount = Arrays.stream(presentInstruction.getPresentCounts()).sum();
+            if (shapeCount * 9 <= gridArea) {
+                solution++;
+            }
+        }
         System.out.printf("The solution to part one is %s.%n", solution);
-    }
-
-    private static void solvePartTwo() throws IOException {
-        long solution = 0;
-        System.out.printf("The solution to part two is %s.%n", solution);
     }
 }
